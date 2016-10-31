db1910a08fddc9b5832ac769a5194d40e24989a4
Christopher Wilson <chriswilson@rtai.co.uk>
2016-10-31 01:12:12 +0000

    Searches for web service directory indexes, downloads all files with specified file extension.

---
+++ IndexOfDownloader/MainControl.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace IndexOfDownloader
+{
+    partial class MainControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.tbSearchTerm = new System.Windows.Forms.TextBox();
+            this.cbSearchExt = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 181F));
+            this.tableLayoutPanel1.Controls.Add(this.btnDownload, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tbUrl, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tbSearchTerm, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cbSearchExt, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(454, 93);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.btnDownload, 3);
+            this.btnDownload.Location = new System.Drawing.Point(3, 63);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(448, 27);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Save .mp3 Files...";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbUrl.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.tbUrl.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.tableLayoutPanel1.SetColumnSpan(this.tbUrl, 3);
+            this.tbUrl.Location = new System.Drawing.Point(3, 37);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(448, 20);
+            this.tbUrl.TabIndex = 2;
+            this.tbUrl.Text = "Index of /... Download URL";
+            this.tbUrl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbUrl_KeyDown);
+            // 
+            // tbSearchTerm
+            // 
+            this.tbSearchTerm.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.tbSearchTerm, 2);
+            this.tbSearchTerm.Location = new System.Drawing.Point(139, 3);
+            this.tbSearchTerm.Name = "tbSearchTerm";
+            this.tbSearchTerm.Size = new System.Drawing.Size(312, 20);
+            this.tbSearchTerm.TabIndex = 1;
+            this.tbSearchTerm.Text = "Enter Search Term...";
+            this.tbSearchTerm.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbSearchTerm_KeyDown);
+            // 
+            // cbSearchExt
+            // 
+            this.cbSearchExt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbSearchExt.FormattingEnabled = true;
+            this.cbSearchExt.Items.AddRange(new object[] {
+            ".mp3",
+            ".flac",
+            ".mp4",
+            ".avi",
+            ".flv",
+            ".mov",
+            ".avi",
+            ".wmv",
+            ".zip"});
+            this.cbSearchExt.Location = new System.Drawing.Point(3, 3);
+            this.cbSearchExt.Name = "cbSearchExt";
+            this.cbSearchExt.Size = new System.Drawing.Size(130, 21);
+            this.cbSearchExt.TabIndex = 0;
+            this.cbSearchExt.Text = "File Extension...";
+            this.cbSearchExt.SelectedIndexChanged += new System.EventHandler(this.cbSearchExt_SelectedIndexChanged);
+            // 
+            // MainControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 118);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.Name = "MainControl";
+            this.Text = "Index of Download helper";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.TextBox tbSearchTerm;
+        private System.Windows.Forms.ComboBox cbSearchExt;
+    }
+}
+
---
+++ IndexOfDownloader/MainControl.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Globalization;
+
+namespace IndexOfDownloader
+{
+    public partial class MainControl : Form
+    {
+        public MainControl()
+        {
+            InitializeComponent();
+            tbUrl.GotFocus += GotFocus_RemoveText;
+            tbSearchTerm.GotFocus += GotFocus_RemoveText;
+        }
+
+
+        public void GotFocus_RemoveText(object sender, EventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                switch (((TextBox)sender).Name)
+                {
+                    case "tbUrl":
+                        tbUrl.Text = "";
+                        tbUrl.GotFocus -= GotFocus_RemoveText;
+                        break;
+                    case "tbSearchTerm":
+                        tbSearchTerm.Text = "";
+                        tbSearchTerm.GotFocus -= GotFocus_RemoveText;
+                        break;
+                }
+            }
+        }
+
+        private void downloadMp3Files(string dlLink, string filePath)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(dlLink, filePath);
+            }
+            string newName = filePath.Replace("%20", " ");
+
+            if (File.Exists(newName))
+            {
+                System.IO.File.Delete(newName);
+            }
+
+            System.IO.File.Move(filePath, newName);
+        }
+
+        private void downloadHandler()
+        {
+            string baseUrl = tbUrl.Text;
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                // Enter key pressed
+                using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+                {
+                    //client.DownloadFile("http://yoursite.com/page.html", @"C:\localfile.html");
+
+                    // Or you can get the file content without saving it:
+                    string htmlCode = client.DownloadString(tbUrl.Text);
+                    //...
+
+                    List<string> fileList = findMp3Files(htmlCode);
+
+                    foreach (string file in fileList)
+                    {
+                        new Task(() => { downloadMp3Files(baseUrl + file, fbd.SelectedPath + @"\" + file); }).Start();
+                        //client.DownloadFile(textBox1.Text + file, fbd.SelectedPath + @"\" + file);
+
+                    }
+                }
+            }
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            Uri uriResult;
+            if (!string.IsNullOrWhiteSpace(tbUrl.Text) && (Uri.TryCreate(tbUrl.Text, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps)))
+            {
+                downloadHandler();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid url.");
+            }
+        }
+        private List<string> findMp3Files(string HtmlCode)
+        {
+            List<string> files = new List<string>();
+            if (!string.IsNullOrWhiteSpace(HtmlCode))
+            {
+                string[] splitCode = HtmlCode.Split('<');
+                foreach (string tagLine in splitCode)
+                {
+                    if (tagLine.IndexOf("href", StringComparison.InvariantCultureIgnoreCase) >= 0)
+                    {
+                        if (tagLine.IndexOf(cbSearchExt.SelectedIndex.ToString(), StringComparison.InvariantCultureIgnoreCase) > 0)
+                        {
+                            var file = new StringBuilder();
+                            bool started = false;
+                            bool ended = false;
+                            for (int i = 0; i < tagLine.Length; ++i)
+                            {
+                                bool startChar = false;
+                                if (tagLine[i] == '"' && !started)
+                                {
+                                    started = true;
+                                    startChar = true;
+                                }
+                                if (tagLine[i] == '"' && started && !startChar)
+                                {
+                                    ended = true;
+                                }
+                                if (started && !ended && !startChar)
+                                {
+                                    file.Append(tagLine[i]);
+                                }
+                            }
+                            files.Add(file.ToString());
+                        }
+                    }
+                }
+            }
+            return files;
+        }
+
+        private void tbUrl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Return)
+            {
+                Uri uriResult;
+                if (!string.IsNullOrWhiteSpace(tbUrl.Text) && (Uri.TryCreate(tbUrl.Text, UriKind.Absolute, out uriResult)
+                    && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps)))
+                {
+                    downloadHandler();
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid url.");
+                }
+            }
+        }
+
+        private void tbSearchTerm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Return)
+            {
+                if (!string.Equals(cbSearchExt.SelectedIndex.ToString(), "Enter Search Term...", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    if (!string.IsNullOrWhiteSpace(tbSearchTerm.Text))
+                    {
+                        System.Diagnostics.ProcessStartInfo sInfo = new System.Diagnostics.ProcessStartInfo();
+                        DialogResult dialogResult = MessageBox.Show("Use search tags?", "Search Type", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (dialogResult == DialogResult.Yes)
+                        {
+                            sInfo = new System.Diagnostics.ProcessStartInfo(
+                                "https://www.google.com/#q=intitle:+index+of+directory+filetype:+" + cbSearchExt.Text
+                                + "+" + tbSearchTerm.Text);
+                        }
+                        else
+                        {
+                            sInfo = new System.Diagnostics.ProcessStartInfo(
+                                "https://www.google.com/#q=index+of+directory" + cbSearchExt.Text
+                                + "+" + tbSearchTerm.Text);
+                        }
+                        System.Diagnostics.Process.Start(sInfo);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a search term.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter or select a file extension from the list.");
+                }
+            }
+        }
+
+        private void cbSearchExt_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (string.Equals(cbSearchExt.SelectedIndex.ToString(), "Enter Search Term...", StringComparison.InvariantCultureIgnoreCase))
+            {
+                MessageBox.Show("Please enter or select a file extension from the list.");
+            }
+            else
+            {
+                tbSearchTerm.Focus();
+            }
+        }
+    }
+}
